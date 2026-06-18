bd7850e5ce67d1015dc4dc6587b2ebd8ab8b96ad
Rachel Feinberg <135255868+rachel-feinberg@users.noreply.github.com>
2026-06-18 12:17:00 -0700

    [BD-6435] Canvas troubleshooting rewrite (#14074)

--- assets/js/broken_redirect_list.js
+++ assets/js/broken_redirect_list.js
@@ -3085,7 +3085,8 @@ validurls['/docs/contributing/content_management/language_translations'] = '/doc
 
 validurls['/docs/developer_guide/sdk_integration/?sdktab=xamarin'] = '/docs/developer_guide/sdk_integration/?sdktab=.net%20maui';
 validurls['/docs/user_guide/message_building_by_channel/in-app_messages/faq/#what-are-templated-in-app-messages'] = '/docs/user_guide/channels/in_app_messages/#templated-in-app-messages';
-validurls['/docs/help/help_articles/campaigns_and_canvas/not_triggering'] = '/docs/user_guide/messaging/canvas/troubleshooting/#why-did-a-user-not-receive-a-triggered-canvas-step';
+validurls['/docs/help/help_articles/campaigns_and_canvas/not_triggering'] = '/docs/user_guide/messaging/canvas/troubleshooting/#user-didnt-receive-a-canvas-message-or-step';
+validurls['/docs/user_guide/messaging/canvas/troubleshooting/#why-did-a-user-not-receive-a-triggered-canvas-step'] = '/docs/user_guide/messaging/canvas/troubleshooting/#user-didnt-receive-a-canvas-message-or-step';
 
 // Access Braze
 validurls['/docs/user_guide/administrative/access_braze/accessing_your_account/#accessing-your-account'] = '/docs/user_guide/administer/personal/accessing_your_account';
